b947daeb77344ffe3d80f4a4bffad22d1e377dd7
mike <mikesapienzaaudio@gmail.com>
2020-10-28 21:00:31 -0500

    colors and edit not working

--- src/index.js
+++ src/index.js
@@ -1,14 +1,45 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-  
-  const newTaskForm = document.getElementById("create-task-form")  
-  const listItem = document.getElementById("tasks")
+  const taskForm = document.getElementById("create-task-form")  
+  const tasks = document.getElementById("tasks")
+  const taskInputField = document.getElementById("new-task-description")
+  const dropdown = document.getElementById('importance-dropdown')
+  const editInputField = document.getElementById("edit-task-description")
+  const editDropdown = document.getElementById('edit-importance-dropdown')
 
-  newTaskForm.addEventListener ('submit', function(e){
+
+
+  function setStyle(importanceValue){
+    let color
+    if (importanceValue === "High") {
+      color = "red"
+    } else if (importanceValue === "Medium") {
+      color = "orange"
+    } else if (importanceValue === "Low") {
+      color = "green"
+    }
+    return color
+  }
+
+  function addToList(inputValue, importanceValue){
+    let LI = document.createElement("LI")
+    LI.innerHTML = inputValue
+    const color = setStyle(importanceValue)
+    let deleteBtn = document.createElement("button")
+    deleteBtn.innerHTML = "X"
+    deleteBtn.className = "remove"
+    let editBtn = document.createElement("button")
+    editBtn.innerHTML = "Edit"
+    editBtn.className = "edit"
+    LI.appendChild(deleteBtn)
+    LI.appendChild(editBtn)
+    tasks.appendChild(LI)
+    taskForm.reset()
+  }
+  
+  taskForm.addEventListener ('submit', function(e){
     e.preventDefault()    
-    let newTask = e.target['new-task-description'].value
-    newTaskForm.reset()
-    listItem.innerHTML += `<li> <div class='todo-task'>${newTask}</div> <button class='remove'> X </button> <button class='edit'> Edit </button> </li>`
+    let userInput = taskInputField.value
+    let importanceValue = dropdown.value
+    addToList(userInput, importanceValue)
   })
 
 
@@ -16,24 +47,33 @@ document.addEventListener("DOMContentLoaded", () => {
     console.log(e.target)
     if (e.target.className === 'remove') {
       e.target.parentElement.remove()
-    } else if (e.target.className === 'edit') {
-      editTodo(e)
-    }
+    } 
+    // else if (e.target.className === 'edit') {
+    //   editTodo(e)
+    // }
   })
 
 
-  function editTodo(e) {
-    let editedTask = e.target.previousElementSibling.previousElementSibling.innerText
-    e.target.parentElement.innerHTML = `<form class="edit-todo" action="#" method="post">
-    <input type="text" id="edit-task-description" name="edit-task-description" value= ${editedTask}>
-    <input type="submit" value="Edit Task">
-    </form>`
-  let editForm = document.querySelector('.edit-todo')
-    editForm.addEventListener ('submit', function(e){
-      e.preventDefault()
-      let editedTask = e.target['edit-task-description'].value
-      e.target.parentElement.innerHTML = `<div class='todo-task'>${editedTask}</div> <button class='remove'> X </button> <button class='edit'> Edit </button>`
-    })
-  }
-
-})
+  // function editTodo(e) {
+  //   debugger
+  //   let editedTask = e.target.previousElementSibling.previousSibling
+  //   // ADD VALUE TO EDIT FORM
+  //   e.target.parentElement.innerHTML = `<form class="edit-todo" action="#" method="post">
+  //   <input type="text" id="edit-task-description" name="edit-task-description">
+  //   <select name="" id="edit-importance-dropdown">
+  //   <option value="High">High</option>
+  //   <option value="Medium">Medium</option>
+  //   <option value="Low">Low</option>
+  //   </select>
+  //   <input type="submit" value="Edit Task">
+  //   </form>`
+  // let editForm = document.querySelector('.edit-todo')
+  //   editForm.addEventListener ('submit', function(e){
+  //     e.preventDefault()
+  //     debugger
+  //     let editUserInput = editInputField.value
+  //     let editImportanceValue = editDropdown.value
+  //     addToList(editUserInput, editImportanceValue)
+  //     // e.target.parentElement.innerHTML = `<div class='todo-task'>${editedTask}</div> <button class='remove'> X </button> <button class='edit'> Edit </button>`
+  //   })
+  // }
